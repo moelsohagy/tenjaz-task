39014f99069a723b82e5c49c36529bec0e06167d
moelsohagy <mohamedelsohagy9@gmail.com>
2024-12-05 11:39:15 +0200

    hanlde layout & routes

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MainHeader from './MainHeader.vue'
+import Sidebar from './Sidebar.vue'
+</script>
+
+<template>
+  <div>
+    <MainHeader />
+
+    <div>
+      <Sidebar />
+
+      <div class="p-4 pt-20 sm:ml-64">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
